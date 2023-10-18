b99d5f826da728d38dffa81f95314294668e74c9
Jounaid Beaufils <jounaidbeaufils@gmail.com>
2023-10-18 17:51:22 +0200

    added comments to anotate where work is needed undo function must work correctly for heuristics this is necessary for a minmax search on a single state deepcopy is slower as it will copy the whole board

--- bokuboard/sumtrackingdict.py
+++ bokuboard/sumtrackingdict.py
@@ -18,3 +18,38 @@ class SumTrackingDict(dict):
     def total(self):
         """Return the sum of all values in the dictionary."""
         return self._total
+
+
+class SumTrackingDictWithUndo(SumTrackingDict):
+    """A dictionary that keeps track of the sum of its values and supports undo operations."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._undo_stack = []
+
+    def __setitem__(self, key, value):
+        # If the key already exists, save its current value to the undo stack, otherwise save None
+        prev_value = self[key] if key in self else None
+        self._undo_stack.append(("set", key, prev_value))
+        
+        super().__setitem__(key, value)
+
+    def __delitem__(self, key):
+        # Save the key and its current value to the undo stack before deletion
+        self._undo_stack.append(("del", key, self[key]))
+        
+        super().__delitem__(key)
+
+    def undo(self):
+        """Revert the last operation."""
+        if not self._undo_stack:
+            raise ValueError("No operations to undo")
+
+        operation, key, prev_value = self._undo_stack.pop()
+
+        if operation == "set":
+            if prev_value is None:  # The key was added in the previous operation, so delete it
+                super().__delitem__(key)
+            else:  # The key was modified in the previous operation, so set it back to its old value
+                super().__setitem__(key, prev_value)
+        elif operation == "del":
+            super().__setitem__(key, prev_value)
